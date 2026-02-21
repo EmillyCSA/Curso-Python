3303d0bf0683020d0578b23414446e76a2683e69
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-02-21 15:20:10 -0300

    Exercícios

--- exerc_input.py
+++ exerc_input.py
@@ -5,3 +5,11 @@ print (input(f"Olá,{nome}!, Seja Bem vinda!"))
 idade = input("Quantos anos você tem?")
 
 print (input(f"Ótimo {idade} anos! Obrigada por informar seu nome e sua idade!"))
+
+
+
+
+
+
+
+
---
+++ exerc_input2.py
@@ -0,0 +1,7 @@
+num1 = int(input(f"Me informe dois números inteiros. Me informe o primeiro numero: "))
+
+num2 = int(input("Me informe o segundo número:   Vou somar e ti dar o resultado."))
+
+resultado = (num1 + num2)
+
+print (f"O resultado é: {resultado}.")
