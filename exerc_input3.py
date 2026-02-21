3a27af6e38a27a96279739f89ba28b4ac770a3d0
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-02-21 16:01:44 -0300

    Exercicio input 3

---
+++ exerc_input3.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Informe a primeira nota: "))
+nota2 = float(input("Informe a segunda nota: "))
+
+
+media = (nota1 + nota2) / 2
+
+media_round = round (media, 1)
+
+print (f"Sua média é {media_round}")
