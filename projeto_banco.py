aa2987a5a7ed8a051cbb1f8780ca0f787b168792
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-03-10 21:48:09 -0300

    INICIO DE PROJETO CAIXA ELETRONICO

---
+++ projeto_banco.py
@@ -0,0 +1,23 @@
+
+
+print (f"Olá, Seja bem vindo ao caixa! ")
+
+opcao1 = "Consultar"
+opcao2 = "Saque"
+opcao3 = "Depósito"
+opcao4 = "Sair"
+
+print (f"1- Consultar saldo")
+print (f"2- Saque")
+print (f"3- Depósito")
+print (f"4- Sair")
+
+opcao = int (input ("Selecione a opção que deseja:"))
+
+saldo = 1000
+
+if opcao == 1:
+ print (f"Seu saldo é: {saldo}")
+
+
+
