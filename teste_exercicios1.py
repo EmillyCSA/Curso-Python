19bfe57068f1ea9d0f5867f9a54bb1abc00e9882
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-06-01 14:39:00 -0300

    Realizando novos testes do Exercicio1.py

---
+++ teste_exercicios1.py
@@ -0,0 +1,8 @@
+temperatura = int (input("Informe a sua temperatura:"))
+
+if temperatura >= 40:
+    print ("Hoje está muito calor!")
+elif (temperatura >=20) and (temperatura <=27):
+    print ("Hoje está fresco!")
+else:
+    print ("Hoje está frio!")
