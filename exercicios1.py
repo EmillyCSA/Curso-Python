d9d2af6f8a489b2f396e20e058d8f7409b7768f4
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-06-01 14:23:52 -0300

    Exercicios de if, elif e else

---
+++ exercicios1.py
@@ -0,0 +1,11 @@
+temperatura = int(input("Informe sua temperatura atual:"))
+
+if temperatura >= 30:
+    print ("Está muito quente!")
+elif (temperatura >=20) and (temperatura <30):
+    print ("Está agradável!")
+else:
+    print ("Está muito frio!")
+
+
+
