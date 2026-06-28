56a5b46ae4a3ce6a9274e71ec7f46a7c53869a69
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-06-28 14:58:50 -0300

    Exercícios de Estrutura de Repetição

---
+++ exercicio_4.py
@@ -0,0 +1,13 @@
+
+#sequencia [1,2,3,4,5]
+#indice = 1
+#indice = 2
+#indice = 3
+#indice = 4
+#indice = 5
+
+
+for indice in range (1, 6):
+    print (f"Executando pela {indice}a vez")
+    print (indice)
+print ("Acabou o loop.")
