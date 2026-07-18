72fbc4889e545656e6ca5ac85142eff92aa39f45
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-07-18 18:05:21 -0300

    Exercício while

---
+++ exercicio_while.py
@@ -0,0 +1,10 @@
+
+#EXERCICIO DE WHILE
+
+senha = input ("Informe a senha: ")
+
+while senha != "emilly2026":
+    print ("Senha incorreta! Tente novamente!")
+    senha = input ("Informe a senha: ")
+
+print ("Senha correta!")
