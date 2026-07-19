4656e063af9dc542050bb7bddc6039edc8dc05a2
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-07-19 13:59:51 -0300

    Exercicios de Lista - Posição Negativa

---
+++ lista.py
@@ -0,0 +1,15 @@
+
+# Exercicios de Lista - Posição negativa
+
+lista = ["Ana", "Thiago", "Liz", "Emilly"]
+
+print (lista)
+
+print (lista [-2])
+print (lista [-3])
+print (lista [-1])
+print (lista [-4])
+print (lista [0])
+
+
+print (f"Tipo de lista: {type (lista)}")
