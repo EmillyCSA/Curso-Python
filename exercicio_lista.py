75fc626401c07ef82d738fce713302d6d2edd2a1
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-07-19 13:42:24 -0300

    Exercicios e treinando Lista posições

---
+++ exercicio_lista.py
@@ -0,0 +1,13 @@
+
+#Exercicio Tipos de Coleções Lista
+
+lista = ["banana", "maçã", "uva", "manga"]
+print (lista)
+
+print (lista[2])
+
+print ("Lista Mercado")
+print (lista[0])
+print (lista[3])
+print (lista[1])
+print (lista[2])
