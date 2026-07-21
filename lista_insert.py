4360ba2e70c3bea913d870240d5aa74665cfbc93
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-07-21 09:03:20 -0300

    #Exercicios de Aprendizagem INSERT

---
+++ lista_insert.py
@@ -0,0 +1,14 @@
+
+#Exercicios de Aprendizagem INSERT
+
+lista = [ "Ana", "Amanda", "Bia", "Carlos", "Cida"]
+
+print (f"Tipo de lista: {type(lista)}")
+
+print ("Lista antes do insert")
+print (lista)
+
+lista.insert (3, "Bela")
+
+print("Lista depois do insert")
+print(lista)
