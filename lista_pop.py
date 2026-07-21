001114f5690f946d1f9d7de82c609056af6684fa
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-07-21 09:56:48 -0300

    Exercicios de Aprendizagem POP

---
+++ lista_pop.py
@@ -0,0 +1,11 @@
+#Exercicios de Aprendizagem POP
+
+lista = ["caju", "uva", "banana","manga"]
+
+print (f"Tipo da lista: {type(lista)}")
+print (lista)
+
+lista.pop (3)
+
+print ("Lista depois do pop:")
+print (lista)
