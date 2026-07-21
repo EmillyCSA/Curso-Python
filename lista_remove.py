6a3dfa1cf8a1ebb51de12a58f21a0a81e5859461
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-07-21 09:40:00 -0300

    Exercicios de Aprendizagem REMOVE

---
+++ lista_remove.py
@@ -0,0 +1,13 @@
+
+#Exercicios de Aprendizagem REMOVE
+
+lista = ["Ana", "Bela", "Carlos","Emilly", "Dani"]
+
+print (f"Tipo de lista: {type(lista)}")
+
+print (lista)
+
+lista.remove ("Emilly")
+
+print ("Lista depois do remove:")
+print (lista)
