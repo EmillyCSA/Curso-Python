ff6d4f42a8215fef7929dbd1980d679519905e2d
Emilly Almeida <emillyalmeida.dev@gmail.com>
2026-07-21 08:56:30 -0300

    Exercicios de Aprendizagem APPEND

---
+++ lista_append.py
@@ -0,0 +1,14 @@
+
+#Exercicios de Aprendizagem APPEND
+
+lista = [ 1, 2, 3, 4]
+
+print (f"Tipo de lista: {type (lista)}")
+
+print ("Lista antes do append")
+print (lista)
+
+lista.append (5)
+
+print ("Lista depois do append")
+print (lista)
